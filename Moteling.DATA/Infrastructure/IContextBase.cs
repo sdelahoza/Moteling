1a27f5579ab5d712092e31156319a08883947b91
sergio.delahoz <sergio.delahoz@gestech.com.co>
2017-04-05 13:39:02 -0500

    Adding DBContext

---
+++ Moteling.DATA/Infrastructure/IContextBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Moteling.DATA.Infrastructure
+{
+    public interface IContextBase
+    {
+        DbSet<TEntity> Set<TEntity>() where TEntity : class;
+
+        EntityEntry<TEntity> Entry<TEntity>(TEntity entity) where TEntity : class;
+
+        int Commit();
+    }
+}
---
+++ Moteling.DATA/MotelingContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moteling.DATA.Configurations;
+using Moteling.DATA.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moteling.DATA
+{
+    public class MotelingContext : DbContext, IContextBase
+    {
+        public MotelingContext(DbContextOptions<MotelingContext> options) : base(options)
+        {
+
+        }
+
+        public int Commit()
+        {
+            return base.SaveChanges();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.AddConfiguration(new RoomImageConfiguration());
+            modelBuilder.AddConfiguration(new RoomConfiguration());
+            modelBuilder.AddConfiguration(new MotelConfiguration());
+            modelBuilder.AddConfiguration(new MotelAddressConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

e7bf22a27204e57655c8a80df940c858ef4b7daf
sergio.delahoz <sergio.delahoz@gestech.com.co>
2017-05-09 13:48:34 -0500

    Get motel by Id

--- Moteling.API/Controllers/MotelController.cs
+++ Moteling.API/Controllers/MotelController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moteling.API.ViewModels;
 using Moteling.DATA.Entities;
+using Moteling.DATA.Extensions;
 using Moteling.DATA.Services.Interfaces;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,28 +16,58 @@ namespace Moteling.API.Controllers
         private readonly ILogger<MotelController> _logger;
         private readonly IMapper _mapper;
         private IEntityService<Motel> _motelService;
-        private IEntityService<RoomImage> _roomImageService;
+        private IEntityService<Room> _roomService;
 
         public MotelController(ILogger<MotelController> logger,
             IMapper mapper,
             IEntityService<Motel> motelService,
-            IEntityService<RoomImage> roomImageService)
+            IEntityService<Room> roomService)
         {
             _logger = logger;
             _mapper = mapper;
             _motelService = motelService;
-            _roomImageService = roomImageService;
+            _roomService = roomService;
         }
 
         [HttpGet]
-        public JsonResult Get()
+        public JsonResult Get(string country = "COLOMBIA", string city = "BARRANQUILLA")
         {
             _logger.LogInformation("Get Method");
 
-            List<Motel> result = _motelService.FindByAndInclude(m => m.Id > 0, m => m.Rooms, m => m.Address).ToList();
+            List<Motel> result = _motelService.FindByAndInclude(
+                m => m.Address.Country == country && m.Address.City == city, 
+                m => m.Address).ToList();
+
+            foreach(var motel in result)
+            {
+                motel.Rooms = _roomService.FindByAndInclude(
+                    room => room.MotelId == motel.Id, 
+                    room => room.Images).ToList();
+            }
+            
             List<MotelVM> motels = _mapper.Map<List<MotelVM>>(result);
 
             return Json(motels);
+        }
+
+        [HttpGet("{id}")]
+        public JsonResult Get(int id)
+        {
+            _logger.LogInformation("Get Motel id={0}", id);
+
+            Motel result = _motelService.FindByAndInclude(
+                m => m.Id == id,
+                m => m.Address).FirstOrDefault();
+
+            if(result == null)
+            {
+                return null;
+            }
+
+            result.Rooms = _roomService.FindByAndInclude(room => room.MotelId == id, room => room.Images).ToList();
+            MotelVM motel = _mapper.Map<MotelVM>(result);
+
+            return Json(motel);
         }
     }
 }

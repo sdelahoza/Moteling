9cd4f56caf202caf414421dfa6dfc83a6cb60150
sergio.delahoz <sergio.delahoz@gestech.com.co>
2017-05-30 13:32:02 -0500

    Agregando nuevamente Moteling.WEB

---
+++ Moteling.WEB/Models/ManageViewModels/FactorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Moteling.WEB.Models.ManageViewModels
+{
+    public class FactorViewModel
+    {
+        public string Purpose { get; set; }
+    }
+}

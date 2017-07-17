1d0f83bb116b4b9cca34c736fd5f6c6b89e7ba0a
Sergio De La Hoz <sergio020893@gmail.com>
2017-07-16 21:03:11 -0500

    Cleaning Home View and Controller

--- Moteling.WEB/Controllers/HomeController.cs
+++ Moteling.WEB/Controllers/HomeController.cs
@@ -17,24 +17,5 @@ namespace Moteling.WEB.Controllers
             _logger.LogInformation("Index page says hello");
             return View();
         }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View();
-        }
     }
 }
